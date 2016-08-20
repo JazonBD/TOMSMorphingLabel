8a0dc5ec5fdaf7dcd8db87bd486de8a3050bef8a
Błażej Szajrych <blazej.szajrych@gmail.com>
2016-08-20 09:34:42 +0200

    Added carthage support

---
+++ TOMSMorphingLabelExample/TOMSMorphingLabel/TOMSMorphingLabel-Umbrella.h
@@ -0,0 +1,20 @@
+//
+//  TOMSMorphingLabel-Umbrella.h
+//  TOMSMorphingLabel-Umbrella
+//
+//  Created by Błażej Szajrych on 20.08.2016.
+//  Copyright © 2016 TomKnig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TOMSMorphingLabel.
+FOUNDATION_EXPORT double TOMSMorphingLabelVersionNumber;
+
+//! Project version string for TOMSMorphingLabel.
+FOUNDATION_EXPORT const unsigned char TOMSMorphingLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TOMSMorphingLabel/PublicHeader.h>
+
+#import <TOMSMorphingLabel/TOMSMorphingLabel.h>
+#import <TOMSMorphingLabel/NSString+Morphing.h>
